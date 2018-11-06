50cea74b3f12990287461081f150cd0ee15f1e10
Jan Bölsche <regular.gonzales@googlemail.com>
2018-11-06 12:29:04 +0100

    Use opts.summary

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const {client} = require('tre-client')
 const Tree = require('tre-treeview')
+const h = require('mutant/html-element')
 
 client( (err, ssb, config) => {
   console.log('tre config', config.tre)
   if (err) return console.error(err)
-  const renderTree = Tree(ssb)
+  const renderTree = Tree(ssb, {
+    summary: kv => h('span', {
+      id: 'key_' + kv.key.substr(1)
+    }, kv.value.content.name)
+  })
   document.body.appendChild(
     renderTree({
       key: config.tre.branches.root,
-      value: {
-        content: {
-          name: 'root'
-        }
-      }
+      value: { content: { name: 'root' } }
     })
   )
 }) 
